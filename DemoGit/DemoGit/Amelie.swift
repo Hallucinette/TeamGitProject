ae7742c6a526a20f38085d62e589f32a2b9cc978
apprenant59 <apprenant59@mar-fmu210021.home>
2022-06-16 11:28:47 +0200

    test de merge

---
+++ DemoGit/DemoGit/Amelie.swift
@@ -0,0 +1,20 @@
+//
+//  Amelie.swift
+//  DemoGit
+//
+//  Created by apprenant59 on 16/06/2022.
+//
+
+import SwiftUI
+
+struct Amelie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Amelie_Previews: PreviewProvider {
+    static var previews: some View {
+        Amelie()
+    }
+}
