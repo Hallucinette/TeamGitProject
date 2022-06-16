78830dfcd95adf5de0071c573515b50667097125
apprenant55 <digital2dreamer@gmail.com>
2022-06-16 12:24:28 +0200

    Sylvain

---
+++ DemoGit/DemoGit/SylvainView.swift
@@ -0,0 +1,20 @@
+//
+//  SylvainView.swift
+//  DemoGit
+//
+//  Created by apprenant55 on 16/06/2022.
+//
+
+import SwiftUI
+
+struct SylvainView: View {
+    var body: some View {
+        Text("View Sylvain")
+    }
+}
+
+struct SylvainView_Previews: PreviewProvider {
+    static var previews: some View {
+        SylvainView()
+    }
+}
