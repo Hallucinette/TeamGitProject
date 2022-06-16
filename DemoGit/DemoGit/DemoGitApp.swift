086276bf44a4b7fb8e4082cb3125102bdcc90c7e
apprenant59 <apprenant59@mar-fmu210021.home>
2022-06-16 10:36:48 +0200

    files

---
+++ DemoGit/DemoGit/DemoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGitApp.swift
+//  DemoGit
+//
+//  Created by apprenant59 on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
